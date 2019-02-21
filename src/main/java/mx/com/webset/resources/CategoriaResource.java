bce911045629ceceae790200fb6534c3f230c3db
Ramiro Barrera <jramiro05@hotmail.com>
2019-02-20 22:49:25 -0600

    Probando primera clase de dominio - Categoria

--- src/main/java/mx/com/webset/resources/CategoriaResource.java
+++ src/main/java/mx/com/webset/resources/CategoriaResource.java
@@ -1,16 +1,30 @@
 package mx.com.webset.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import mx.com.webset.dominio.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 
 	@RequestMapping( method=RequestMethod.GET )
-	public String listado() {
-		return "¡REST funcionando!";
+	public List<Categoria> listado() {
+		
+		Categoria cat1 = new Categoria(1, "INFORMATICA");
+		Categoria cat2 = new Categoria(2, "ESCRITORIO");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		return lista;
 	}
 	
 }
