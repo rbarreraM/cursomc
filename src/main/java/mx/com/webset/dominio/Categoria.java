4ffa96c081ac839ec1af73e1513eff06dcbd1be5
Ramiro Barrera <jramiro05@hotmail.com>
2019-02-20 23:10:59 -0600

    Base de datos H2H y creacion automatica de base de datos

--- src/main/java/mx/com/webset/dominio/Categoria.java
+++ src/main/java/mx/com/webset/dominio/Categoria.java
@@ -2,13 +2,21 @@ package mx.com.webset.dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id; 
 	private String nombre;
-
+	
 	public Categoria() {
 		super();
 	}
@@ -18,7 +26,7 @@ public class Categoria implements Serializable{
 		this.id = id;
 		this.nombre = nombre;
 	}
-
+	
 	public Integer getId() {
 		return id;
 	}
