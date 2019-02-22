ca366d06703ed42e4ef3c51ccda1f758478c8dbe
Ramiro Barrera <jramiro05@hotmail.com>
2019-02-21 22:57:01 -0600

    Creacion de operacion de instanciacion

--- src/main/java/mx/com/webset/CursomcApplication.java
+++ src/main/java/mx/com/webset/CursomcApplication.java
@@ -1,13 +1,33 @@
 package mx.com.webset;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class CursomcApplication {
+import mx.com.webset.dominio.Categoria;
+import mx.com.webset.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class CursomcApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+
+		Categoria obj1 = new Categoria( null, "Informatica");
+		Categoria obj2 = new Categoria( null, "Escritorio");
+		
+		categoriaRepository.saveAll( Arrays.asList(obj1, obj2 ));
+		
 	}
 
 }
